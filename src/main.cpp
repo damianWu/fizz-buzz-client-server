6494c693af0dbd954c2de743bbc49ddacdc74388
@damianWu <damian.wolonkiewicz@gmail.com>
2022-05-12 21:21:35 +0200

    Added server code to main()

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,22 @@
 // Copyright [2022] <@damianWu>
 
+#include <sys/types.h>
+
 #include <clocale>
+#include <cstdint>
 #include <iostream>
 
-// #include "server/server.hpp"
 #include "client/client.hpp"
+#include "server/server.hpp"
+#include "server/server/server.hpp"
 
 int main() {
     try {
-        setlocale(LC_ALL, "polish");
-        client::run_client("localhost", 11234);
+        uint16_t port{11234};
+        server::run_server(port);
+
+        // setlocale(LC_ALL, "polish");
+        client::run_client("localhost", port);
     } catch (const std::exception& e) {
         std::cerr << "Exception catch in main function with message: "
                   << e.what() << '\n';
