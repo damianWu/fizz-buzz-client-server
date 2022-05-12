ba1cd2a60b4841a3ee5b38bdeda1291f5227c7e0
@damianWu <damian.wolonkiewicz@gmail.com>
2022-05-12 17:12:44 +0200

    Session class basic impl.

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -1,3 +1,39 @@
 // Copyright [2022] <@damianWu>
 
 #include "server/server.hpp"
+
+#include "asio/ip/tcp.hpp"
+
+namespace server {
+
+Session::Session(asio::ip::tcp::socket tcp_socket_)
+    : tcp_socket_{std::move(tcp_socket_)} {}
+
+std::shared_ptr<Session> Session::get_ptr() { return shared_from_this(); }
+
+[[nodiscard]] std::shared_ptr<Session> Session::create(
+    asio::ip::tcp::socket tcp_socket) {
+    return std::shared_ptr<Session>(new Session(std::move(tcp_socket)));
+}
+
+/* ========================================================================== */
+
+std::string fizz_buzz(const int number) {
+    if (number != 0) {
+        int mod3{number % 3};
+        int mod5{number % 5};
+
+        if (mod3 == 0 && mod5 == 0) {
+            return "fizz-buzz";
+        }
+        if (mod3 == 0) {
+            return "fizz";
+        }
+        if (mod5 == 0) {
+            return "buzz";
+        }
+    }
+    return std::to_string(number);
+}
+
+}  // namespace server
--- src/server/server/server.hpp
+++ src/server/server/server.hpp
@@ -2,4 +2,28 @@
 #ifndef SRC_SERVER_SERVER_SERVER_HPP_
 #define SRC_SERVER_SERVER_SERVER_HPP_
 
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "asio.hpp"
+
+namespace server {
+
+class Session : public std::enable_shared_from_this<Session> {
+    std::shared_ptr<Session> get_ptr();
+
+    [[nodiscard]] static std::shared_ptr<Session> create(asio::ip::tcp::socket);
+
+ private:
+    explicit Session(asio::ip::tcp::socket);
+
+    std::vector<char> data_;
+    asio::ip::tcp::socket tcp_socket_;
+};
+
+std::string fizz_buzz(int number);
+
+}  // namespace server
+
 #endif  // SRC_SERVER_SERVER_SERVER_HPP_
