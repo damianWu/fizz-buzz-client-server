307d87be8e147886974e3f0bb57ac33d2cb719c2
@damianWu <damian.wolonkiewicz@gmail.com>
2022-05-12 21:17:07 +0200

    main() prepared for client

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,15 @@
 // Copyright [2022] <@damianWu>
 
+#include <clocale>
 #include <iostream>
 
-#include "server/server.hpp"
+// #include "server/server.hpp"
+#include "client/client.hpp"
 
 int main() {
     try {
-        server::run_server(11234);
+        setlocale(LC_ALL, "polish");
+        client::run_client("localhost", 11234);
     } catch (const std::exception& e) {
         std::cerr << "Exception catch in main function with message: "
                   << e.what() << '\n';
