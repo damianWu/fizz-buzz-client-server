8d131eeebd00fe35a3f5ad37af739b74cbb4ac41
@damianWu <damian.wolonkiewicz@gmail.com>
2022-05-12 17:44:31 +0200

    Implement. of Session::write() and Session::read()

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -2,9 +2,44 @@
 
 #include "server/server.hpp"
 
+#include <iostream>
+// #include <ostream>
+#include <system_error>
+
 #include "asio/ip/tcp.hpp"
 
 namespace server {
+
+void Session::read() {
+    auto self{get_ptr()};
+
+    tcp_socket_.async_read_some(
+        asio::buffer(data_, data_.size()),
+        [this, self](const std::error_code ec,
+                     const std::size_t bytes_transferred) {
+            if (!ec) {
+                auto number{std::string(data_.data(), bytes_transferred)};
+                auto result{fizz_buzz(std::atoi(number.c_str()))};
+                std::cout << "number: " << result << std::endl;
+                write(result);
+            }
+        });
+}
+
+void Session::write(const std::string_view response) {
+    auto self{get_ptr()};
+
+    tcp_socket_.async_write_some(
+        asio::buffer(response.data(), response.length()),
+        [this, self](const std::error_code ec,
+                     const std::size_t bytes_transferred) {
+            if (!ec) {
+                read();
+            }
+        });
+}
+
+void Session::start() { read(); }
 
 Session::Session(asio::ip::tcp::socket tcp_socket_)
     : tcp_socket_{std::move(tcp_socket_)} {}
