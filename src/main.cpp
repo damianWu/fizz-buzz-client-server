b1c39a796a827158252985534c755677e19827ca
@damianWu <damian.wolonkiewicz@gmail.com>
2022-05-12 20:42:40 +0200

    Impl. server in main()

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,17 @@
 
 #include <iostream>
 
+#include "server/server.hpp"
+
 int main() {
     try {
+        server::run_server(11234);
     } catch (const std::exception& e) {
         std::cerr << "Exception catch in main function with message: "
                   << e.what() << '\n';
     } catch (...) {
         std::cerr << "Unknown type of exception catch in main function" << '\n';
     }
+    std::cout << "main() end reached" << '\n';
     return 0;
 }
