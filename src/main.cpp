cb6f58d68b06ce35f2a159c374aff4d3080e1f3e
@damianWu <damian.wolonkiewicz@gmail.com>
2022-05-13 15:48:32 +0200

    Refactor main() function

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,17 @@
 // Copyright [2022] <@damianWu>
 
-#include <sys/types.h>
-
 #include <clocale>
 #include <cstdint>
 #include <iostream>
 
 #include "client/client.hpp"
-// #include "server/server.hpp"
+#include "server/server.hpp"
 
 int main() {
     try {
-        uint16_t port{5235};
-        // server::run_server(port);
-
-        client::run_client("localhost", port);
+        auto port{9970u};
+        server::run_server(port);
+        // client::run_client("localhost", port);
     } catch (const std::exception& e) {
         std::cerr << "Exception catch in main function with message: "
                   << e.what() << '\n';
